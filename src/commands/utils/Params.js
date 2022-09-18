38cdc44f739f06725938b3489df2f35205ef98f5
Luis Ricardo Alves Santos <luisricardo2825@gmail.com>
2022-09-17 22:32:24 -0300

    Correção de async desnecessário

--- src/commands/utils/Params.js
+++ src/commands/utils/Params.js
@@ -2,12 +2,12 @@ import * as fs from "fs";
 import path from "path";
 import { checkIfFileExists } from "./file.js";
 
-export const ParamsFile = async (DirPath) => {
+export const ParamsFile = (DirPath) => {
   const filePath = path.join(DirPath, "params.json");
 
   if (checkIfFileExists(filePath)) return JSON.parse(fs.readFileSync(filePath));
 
   fs.writeFileSync(filePath, JSON.stringify({}));
-  
+
   return JSON.parse(fs.readFileSync(filePath));
 };
