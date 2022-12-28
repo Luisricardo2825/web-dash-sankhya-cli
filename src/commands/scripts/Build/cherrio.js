dfbbbd3f55f593ce2bd25339ceb8c810735795f5
Luis Ricardo Alves Santos <luisricardo2825@gmail.com>
2022-12-28 17:24:19 -0300

    Correção de substituição indevida da tag link

--- src/commands/scripts/Build/cherrio.js
+++ src/commands/scripts/Build/cherrio.js
@@ -2,6 +2,8 @@ import * as fs from "fs";
 import * as cheerio from "cheerio";
 import { ParamsFile } from "../../utils/index.js";
 
+const pattern = /[A-Za-z]/; // Somente letras
+
 export async function Sanitizehtml(file, spinner, currentPath) {
   const parameters = JSON.stringify(ParamsFile(currentPath, "cherrriooo"));
   var $ = cheerio.load(fs.readFileSync(file), {
@@ -12,13 +14,17 @@ export async function Sanitizehtml(file, spinner, currentPath) {
   spinner.color = "cyan";
   spinner.text = `Alterando caminho dos links...`;
   $("head link").each(async function (i, elm) {
-    $(this).attr("href", "${BASE_FOLDER}" + $(this).attr("href"));
+    const attr = $(this).attr("href");
+
+    if (pattern.test(attr[0]))
+      $(this).attr("href", "${BASE_FOLDER}" + $(this).attr("href"));
   });
 
   spinner.color = "yellow";
   spinner.text = `Alterando caminho dos scripts...`;
   $("head script").each(async function (i, elm) {
-    if ($(this).attr("src")) {
+    const attr = $(this).attr("src");
+    if ($(this).attr("src") && pattern.test(attr[0])) {
       $(this).attr("src", "${BASE_FOLDER}" + $(this).attr("src"));
     }
   });
